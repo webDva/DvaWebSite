efa7af6ca73b6a613883969041f80e8c73e44daf
jermaine <jaylarcane@gmail.com>
2017-06-22 13:50:58 -0400

    removed app seed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
 import { PortfolioGridListComponent } from './portfolio-grid-list/portfolio-grid-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     PortfolioGridListComponent
   ],
   imports: [
